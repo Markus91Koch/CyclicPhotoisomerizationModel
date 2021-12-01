99390f51512f89871fb4d586b068590ed54b5c40
Markus <markus91koch@gmail.com>
2021-12-01 17:50:26 +0100

    added docmentation for tools concatenate and continuum

--- analyis_scripts/concatenate.sh
+++ analyis_scripts/concatenate.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-#cp trimer1.dcd new_trimer1_.dcd
-#/home/koch_local/catdcd/LINUX/bin/catdcd4.0/catdcd -o new_trimer1.dcd -first 2 trimer1.dcd
-#catdcd="/scratch/s4688360/catdcd/LINUX/bin/catdcd4.0/catdcd"
+# Tool to concatenate the separate but consecutive MD trajectories (DCD format),
+# which were created during the simulations. This allows the analysis of one 
+# large file instead of several. The tool requires the free
+# software catdcd, see: https://www.ks.uiuc.edu/Development/MDTools/catdcd/
+
+#########################################
+# Place this script in the parent folder of
+# the directories which contain the dcd files.
+# Execute as "sh continuum.sh"
+
+
+# path to catdcd executable
 catdcd="/scratch/ws/0/s4688360-misc/catdcd/LINUXAMD64/bin/catdcd4.0/catdcd"
 
+# general name of the dcd files
 names=$(find . -type f -name 'unwrap*dcd' | sort -n -t _ -k 2)
 
+# name where to save the concatenated trajectory
 foldername="extract_dcd"
 
+# overview of dcd files
 echo $names | tail -1
-
-#exit
 
 mkdir -p "$foldername"
 
 i=0
 var=""
 for item in $names; do
+
 	echo $item
 	extension="${item##*.}"
 	filename="${item%.*}"
 	end="${filename##*_}"
 	thestart="${filename%_*}"
 	nr="${thestart##*_}"
+
 	echo $extension
 	echo $filename
 	echo $end
 	echo $nr
+
 	if [ $i -eq 0 ]; then
 		cp $item new_"$nr"_"$end"."$extension"
 		mv new_"$nr"_"$end"."$extension" "$foldername"
@@ -35,16 +48,17 @@ for item in $names; do
 		$catdcd -o new_"$nr"_"$end"."$extension" -first 2 $item 
 		mv new_"$nr"_"$end"."$extension" "$foldername"
 	fi
+
 	i=$((i+1))
 	echo $i
 	var="${var} new_${nr}_${end}.${extension}"
-	#echo $var
 done
 
 cd "$foldername"
 
 echo "$var"
 
+# connect separate files to one DCD file
 $catdcd -o tot.dcd $var
 rm new*.dcd
 
